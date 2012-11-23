2745255abacc526ffa64b6bf1760cc374245e219
dobabeswe <chololoy@gmail.com>
2012-11-23 12:11:45 -0600

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,19 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Cafe Hobo'" do
+    it "should have the h1 'Cafe Hobo'" do
       visit '/static_pages/home'
-      page.should have_content('Cafe Hobo')
+      page.should have_selector('h1', :text => 'Cafe Hobo')
     end
 
-    it "should have the title 'Home'" do 
+    it "should have the base title" do 
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Cafe Hobo | Home")
+      page.should have_selector('title', :text => "Cafe Hobo")
+    end
+
+    it "should not have a custom page title" do 
+      visit 'static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 end
